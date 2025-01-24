4a31c901c630c11bec5090b4f5e18dfe0f1f0bc2
offor <offorchukwuebuka3@gmail.com>
2025-01-24 21:25:54 +0100

    first commit

--- src/layouts/components/nav-upgrade.tsx
+++ src/layouts/components/nav-upgrade.tsx
@@ -1,10 +1,8 @@
 import type { StackProps } from '@mui/material/Stack';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-import { textGradient } from 'src/theme/styles';
 
 // ----------------------------------------------------------------------
 
@@ -17,38 +15,20 @@ export function NavUpgrade({ sx, ...other }: StackProps) {
       sx={{ mb: 4, textAlign: 'center', ...sx }}
       {...other}
     >
-      <Typography
-        variant="h6"
-        sx={(theme) => ({
-          ...textGradient(
-            `to right, ${theme.vars.palette.secondary.main}, ${theme.vars.palette.warning.main}`
-          ),
-        })}
-      >
-        More features?
-      </Typography>
+     
       <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5 }}>
-        {`From only `}
-        <Box component="strong" sx={{ color: 'text.primary' }}>
-          $69
-        </Box>
+        {`Working In Progress `}
+    
       </Typography>
 
       <Box
         component="img"
-        alt="Minimal dashboard"
+        alt="Soft Invites"
         src="/assets/illustrations/illustration-dashboard.webp"
         sx={{ width: 200, my: 2 }}
       />
 
-      <Button
-        href="https://material-ui.com/store/items/minimal-dashboard/"
-        target="_blank"
-        variant="contained"
-        color="inherit"
-      >
-        Upgrade to Pro
-      </Button>
+     
     </Box>
   );
 }
