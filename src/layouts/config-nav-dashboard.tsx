866d4fbbce785c88dadf209555242cd937e73bd9
Stanley <offorstanley4real@gmail.com>
2026-06-17 21:43:56 +0100

    removed duplicate email logic

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -17,11 +17,11 @@ export const navData = [
     path: '/event',
     icon: icon('ic-user'),
   },
-  // {
-  //   title: 'WhatsApp Templates',
-  //   path: '/whatsapp-templates',
-  //   icon: icon('ic-cart'),
-  // },
+  {
+    title: 'WhatsApp Templates',
+    path: '/whatsapp-templates',
+    icon: icon('ic-cart'),
+  },
 
     {
     title: 'Change Password',
